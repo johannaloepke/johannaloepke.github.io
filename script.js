592d0b0e7a6929850348fcf3b09bc43a7df99143
johannaloepke <johanna.loepke@gmail.com>
2016-10-15 21:58:44 -0400

    Added NASA's API to the jumbotron

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+	// creates a smooth scrolling effect for the different pages
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
@@ -11,4 +13,23 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
+
+	// changes the jumbotron picture to NASA's Astronomy Picture of the Day on hover
+	$('.jumbotron').hover( 
+		function() {
+			$.ajax("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", {
+	    		success: function (data) {
+	      			$(".jumbotron").css("background", "url(" + data.url + ")");
+	    		}
+	    	});
+		},
+		function() {
+			$(".jumbotron").css("position", "relative");
+			$(".jumbotron").css("background", "#000 url(\"ocean.jpg\") center center");
+			$(".jumbotron").css("width", "100%");
+			$(".jumbotron").css("height", "500px");
+			$(".jumbotron").css("background-size", "cover");
+			$(".jumbotron").css("overflow", "hidden");
+		}
+	);
 });
