0b2d05ef8283ed0fe8a32dcd441472f1a02ea2fd
johannaloepke <johanna.loepke@gmail.com>
2017-09-20 11:49:14 -0700

    fixed bug where jumbotron height changes after reverting to original picture

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 			$(".jumbotron").css("position", "relative");
 			$(".jumbotron").css("background", "#000 url(Images/ocean.jpg) center center");
 			$(".jumbotron").css("width", "100%");
-			$(".jumbotron").css("height", "56vw");
+			$(".jumbotron").css("height", "100vw");
 			$(".jumbotron").css("background-size", "cover");
 			$(".jumbotron").css("overflow", "hidden");
 		}
