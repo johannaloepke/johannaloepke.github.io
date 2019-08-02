4917c1f14359639969a22522b7291f104ca9bb2c
Johanna Loepke <loepkej@ccs.neu.edu>
2019-08-02 07:52:55 -0700

    add fade in animations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-	// creates a smooth scrolling effect for the different pages
-	$('a[href^="#"]').on('click',function (e) {
+	// Creates a smooth scrolling effect to jump to different tags
+	$('a[href^="#"]').on('click',function(e) {
 	    e.preventDefault();
 
 	    var target = this.hash,
@@ -14,7 +14,19 @@ $(document).ready(function() {
 	    });
 	});
 
-	// changes the jumbotron picture to NASA's Astronomy Picture of the Day on click
+	// Fades in elements as the user scrolls
+	$(window).scroll(function() {
+		$('.fadeIn').each(function(i){
+			var middle_of_object = $(this).position().top + $(this).outerHeight() / 3;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+				   
+			if(bottom_of_window > middle_of_object) {
+				$(this).animate({'opacity':'1'}, 1000);	  
+			}  
+		}); 
+	});
+
+	// Changes the jumbotron picture to NASA's Astronomy Picture of the Day on click
 	$('.jumbotron').toggle( 
 		function() {
 			$.ajax("https://api.nasa.gov/planetary/apod?hd=False&api_key=ffNevQSLNePBIdPvXmyuJHsmjnnAi4jYvWJGELsK", {
@@ -40,11 +52,13 @@ $(document).ready(function() {
 		}
 	);
 
-    // shows course descriptions when clicking the title
+    // Shows course descriptions when clicking the title
     $(function() {
-    	$('#descriptions').find('.toggle').click(function(){
+    	$('#descriptions').find('.toggle').click(function() {
       	$(this).next().slideToggle('fast');
       	$(".content").not($(this).next()).slideUp('fast');
-    });
+	});
+	
+	// TODO lazy load images
   });
 });
